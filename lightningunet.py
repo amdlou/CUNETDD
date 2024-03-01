37662fbffb98516268eef6578a0e65762d231bc3
anveshnathaniou <123520138+anveshnathaniou@users.noreply.github.com>
2024-02-29 19:45:27 -0600

    Update lightningunet.py

--- lightningunet.py
+++ lightningunet.py
@@ -1,246 +1,177 @@
-import matplotlib.pyplot as plt
-import os
-import numpy as np
+from re import T
 import torch
+import torch.fft
 import torch.nn as nn
 import torch.nn.functional as F
-import pytorch_lightning as pl
-from argparse import Namespace
-from fcunet import ComplexUNet
-from loss import CustomSSIMLoss
-from datautils import ParseDataset
-#from dataset import ParseDataset
-from torch.utils.data import DataLoader
-from torch.utils.data import random_split
-#import lightning as L
-
-def normalize_image(image):
-        max_val = np.max(image)
-        if max_val > 0:
-            return image / max_val
-        return image
 
 
-class ComplexUNetLightning(pl.LightningModule):
-    def __init__(self, input_channel, image_size, filter_size, n_depth, dp_rate=0.3, activation=nn.ReLU,batch_size=256):
-        super(ComplexUNetLightning, self).__init__()
-        self.complex_unet = ComplexUNet(input_channel, image_size, filter_size, n_depth, dp_rate, activation)
-        self.batch_size = batch_size
-        self.loss=[]
-        self.epochs=[]
-        #self.all_targets=[]
-        #self.all_outputs=[]
-        self.targets=[]
-        self.outputs=[]
-        self.sample_counter = 0
-        
-        self.loss_fn = CustomSSIMLoss
-        self.los_fn=F.mse_loss
+def cross_correlate_fft(cb, pr):
+    # Assuming cb and pr are torch tensors in the shape of (batch, channel, height, width)
 
-    def forward(self, inputsA, inputsB):
-        return self.complex_unet(inputsA, inputsB)
+    # Shift the probe and CBED patterns to the origin
+    pr_shifted = torch.fft.ifftshift(pr, dim=(-2, -1))
+    cb_shifted = torch.fft.ifftshift(cb, dim=(-2, -1))
+
+    # Perform FFT
+    cbed_fft = torch.fft.fft2(cb_shifted.to(torch.complex64))
+    probe_fft = torch.fft.fft2(pr_shifted.to(torch.complex64))
+
+    # Multiply CBED FFT with the conjugate of probe FFT
+    ccff = cbed_fft * torch.conj(probe_fft)
+
+    # Normalize each cross-correlation
+    ccff_norm = torch.linalg.vector_norm(ccff, dim=(-2, -1), keepdim=True)
+    ccff_normalized = ccff / ccff_norm
+
+    # Split into real and imaginary parts and concatenate along the channel dimension
+    ccff_real = ccff_normalized.real
+    ccff_imag = ccff_normalized.imag
+    ccff_combined = torch.cat([ccff_real, ccff_imag], dim=1)  # Concatenating on channel dimension
+
+    return ccff_combined
+
+def cross_correlate_ifft(x):
+    # Assuming x is a torch tensor in the shape of (batch, 2 * channel, height, width)
+    # where first half channels are real and second half are imaginary parts
+
+    input_channel = x.size(1) // 2
+    input_complex = torch.complex(x[:, :input_channel, :, :], x[:, input_channel:, :, :])
+
+    # Perform IFFT and shift
+    output_complex = torch.fft.fftshift(torch.fft.ifft2(input_complex), dim=(-2, -1))
+
+    # Take the real part
+    output = output_complex.real
+
+    return output
+
+
+class MonteCarloDropout(nn.Module):
+    def __init__(self, p=0.1):
+        super(MonteCarloDropout, self).__init__()
+        self.p = p
+
+    def forward(self, inputs):
+        # Apply dropout both in training and evaluation modes
+        return F.dropout(inputs, self.p, training=True)
+
+
+class Conv2D(nn.Module):
+    def __init__(self, in_channels, n_filters, n_depth=2, kernel_size=3, activation=nn.ReLU, dp_rate=0.1, batchnorm=True,bias = True):
+        super(Conv2D, self).__init__()
+        self.n_depth = n_depth
+        self.activation = activation
+        self.dp_rate = dp_rate
+        self.batchnorm = batchnorm
+
+        self.layers = nn.ModuleList()
+        for _ in range(n_depth):
+            self.layers.append(nn.Conv2d(in_channels, n_filters, kernel_size, padding='same',bias=True ))
+            if batchnorm:
+                self.layers.append(nn.BatchNorm2d(n_filters))              
+            if activation is not None:
+                self.layers.append(activation())
+            self.layers.append(MonteCarloDropout(dp_rate))
+            in_channels = n_filters  # Output channels become input for the next layer
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+
+class ConvComplex2D_py(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding='same', dilation=1, groups=1, bias=True):
+        super(ConvComplex2D_py, self).__init__()
+
+        # No need to adjust in_channels and out_channels for real and imaginary parts
+        # Both the real and imaginary convolutions will now output out_channels channels each
+
+        # Initialize convolution layers for real and imaginary parts
+        self.real_conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias)
+        self.imag_conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias)
+
+    def forward(self, input):
+        # Assume input is of shape [batch_size, 2*in_channels, height, width]
+        # Where the first half of the channels are real and the second half are imaginary
+        real_input, imag_input = torch.chunk(input, 2, dim=1)
+
+        # Perform convolution on real and imaginary parts separately
+        real_output = self.real_conv(real_input) - self.imag_conv(imag_input)
+        imag_output = self.imag_conv(real_input) + self.real_conv(imag_input)
+
+        # Concatenate the real and imaginary parts in the channel dimension
+        # Now, both real and imaginary outputs have out_channels channels, doubling the output channels
+        return torch.cat([real_output, imag_output], dim=1)
+
+class ComplexUpsample2d(nn.Module):
+    def __init__(self, scale_factor=2, mode='bilinear', align_corners=False):
+        super(ComplexUpsample2d, self).__init__()
+        # Define the upsampling parameters
+        self.scale_factor = scale_factor
+        self.mode = mode
+        self.align_corners = align_corners if mode in ['linear', 'bilinear', 'bicubic', 'trilinear'] else None
+ 
+    def forward(self, x):
+        # Split input tensor into real and imaginary parts based on the channel dimension
+        real_input, imag_input = torch.chunk(x, 2, dim=1)
+ 
+        # Apply upsampling separately to real and imaginary components
+        real_output = F.interpolate(real_input, scale_factor=self.scale_factor, mode=self.mode, align_corners=self.align_corners)
+        imag_output = F.interpolate(imag_input, scale_factor=self.scale_factor, mode=self.mode, align_corners=self.align_corners)
+ 
+        # Concatenate the outputs to maintain the structure
+        output = torch.cat([real_output, imag_output], dim=1)
+        return output
+
+
+
+
+class ComplexConvTranspose2d(nn.Module):
+    def __init__(self, in_channel, out_channel, kernel_size, stride=1, padding=0, output_padding=0, dilation=1, groups=1, bias=True, **kwargs):
+        super(ComplexConvTranspose2d, self).__init__()
+        # Define transposed convolution for real and imaginary separately
+        self.tconv_re = nn.ConvTranspose2d(in_channel, out_channel, kernel_size, stride, padding, output_padding, groups, bias, dilation)
+        self.tconv_im = nn.ConvTranspose2d(in_channel, out_channel, kernel_size, stride, padding, output_padding, groups, bias, dilation)
+
+    def forward(self, x):
+        # Split input tensor into real and imaginary parts based on the channel dimension
+        real_input, imag_input = torch.chunk(x, 2, dim=1)
+
+        # Apply transposed convolution separately and perform complex arithmetic
+        real_output = self.tconv_re(real_input) - self.tconv_im(imag_input)
+        imag_output = self.tconv_im(real_input) + self.tconv_re(imag_input)
+
+        # Concatenate the outputs to maintain the structure
+        output = torch.cat([real_output, imag_output], dim=1)
+        return output
     
-    def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True,drop_last=True,num_workers=0,pin_memory=True)
-    
-    def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=False,drop_last=True,num_workers=0,pin_memory=True)
 
-    def test_dataloader(self):
-        return DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False,drop_last=True)
-    #def on_training_epoch_start(self):
-        #print("Validation epoch start")
-        #self.targets.clear()  # Clear the list of targets at the start of validation epoch
-        #self.outputs.clear()  # Clear the list of outputs at the start of validation epoch
-        #self.sample_counter = 0  # Reset the sample counter at the start of validation epoch
-    
-    def training_step(self, batch, batch_idx):
-        inputsA, inputsB, targets = batch
-        #print(inputsA.shape,inputsB.shape,targets.shape)
-        outputs = self(inputsA, inputsB)
-        #print(f"Output: {outputs.size()}")
-        
-        loss_1 =self.loss_fn(targets, outputs)
-        self.log('Train_loss_1', loss_1)
-        loss_2 = self.los_fn(targets, outputs)
-        self.log('Train_loss_2', loss_2)
-        loss=loss_1+loss_2
-        self.log('Train_loss', loss)
-        
-        #print(f"Loss: {loss}")
-        return {'loss': loss}
-    
-    def on_validation_epoch_start(self):
-        #print("Validation epoch start")
-        self.targets.clear()  # Clear the list of targets at the start of validation epoch
-        self.outputs.clear()  # Clear the list of outputs at the start of validation epoch
-        self.sample_counter = 0  # Reset the sample counter at the start of validation epoch
-        #make sure that the lists are empty
-        #print(f"Targets: {len(self.targets)}, Outputs: {len(self.outputs)}")
-        #print(f"Sample counter: {self.sample_counter}")
+class ConvSpec2D(nn.Module):
+    def __init__(self, in_channels, n_filters, n_depth=1, kernel_size=3, activation=nn.ReLU, dp_rate=0.1, batchnorm=True, bias=True):
+        super(ConvSpec2D, self).__init__()
+        self.layers = nn.ModuleList()
 
-        
-    def validation_step(self, batch, batch_idx):
-        inputsA, inputsB, targets = batch
-        outputs = self(inputsA, inputsB)
-        loss_1 =self.loss_fn(targets, outputs)
-        self.log('val_loss_1', loss_1)
-        loss_2 = self.los_fn(targets, outputs)
-        self.log('val_loss_2', loss_2)
-        loss=loss_1+loss_2
-        loss = F.mse_loss(targets, outputs)
-        self.log('val_loss', loss) 
-        #samples should be cleared before each epoch      
-        MAX_SAMPLES = 500
-        samples_to_collect = min(targets.size(0), MAX_SAMPLES - self.sample_counter)
-        if samples_to_collect > 0:
-            self.targets.append(targets[:samples_to_collect].detach())
-            self.outputs.append(outputs[:samples_to_collect].detach())
-            self.sample_counter += samples_to_collect
-        #print(f"Sample counter: {self.sample_counter}")
-        
-        return {'val_loss': loss}
+        #if batchnorm:
+            # Apply BN to the input channels before convolution
+            #self.layers.append(nn.BatchNorm2d(in_channels))
 
-    def test_step(self, batch, batch_idx):
-        inputsA, inputsB, targets = batch
-        outputs = self(inputsA, inputsB)
-        loss_1=self.loss_fn(targets, outputs)
-        self.log('test_loss_1', loss_1)
-        loss_2 = self.los_fn(targets, outputs)
-        self.log('test_loss_2', loss_2)
-        loss=loss_1+loss_2
-        self.log('test_loss', loss)
-        MAX_SAMPLES = 500
-        samples_to_collect = min(targets.size(0), MAX_SAMPLES - self.sample_counter)
-        if samples_to_collect > 0:
-            self.targets.append(targets[:samples_to_collect].detach())
-            self.outputs.append(outputs[:samples_to_collect].detach())
-            self.sample_counter += samples_to_collect
-        #self.targets.append(targets.detach())
-        #self.outputs.append(outputs.detach())
-        return {'test_loss': loss}
-        
+        for _ in range(n_depth):
+            conv_layer = ConvComplex2D_py(in_channels, n_filters, kernel_size, padding='same',bias=bias )
+            self.layers.append(conv_layer)
 
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
-        return optimizer
-    
-    def setup(self, stage=None):
-        # Load the full dataset
-        torch.manual_seed(42)
-        full_dataset = ParseDataset(filepath='3fb20ba5-ed3d-4c55-9e35-cfaa97b85cdd_training.h5')
-        full_dataset.read(batch_size=self.batch_size, shuffle=True, mode='default', task='system', one_hot=False)
-        # Split the dataset into training, validation, and test datasets
-        train_size = int(0.7 * len(full_dataset))  # Use 70% of the data for training
-        val_size = int(0.15 * len(full_dataset))  # Use 15% of the data for validation
-        test_size = len(full_dataset) - train_size - val_size
-        print(f"Train size: {train_size}, Val size: {val_size}, Test size: {test_size}")
-                
-        self.train_dataset, self.val_dataset, self.test_dataset = random_split(full_dataset, [train_size, val_size, test_size])
-    
-    
-    def on_train_epoch_end(self, num_images_to_plot=2):
-        
-        avg_loss = self.trainer.callback_metrics['Train_loss']
-        if isinstance(avg_loss, torch.Tensor):
-           avg_loss = avg_loss.cpu().numpy()
-    
-        self.loss.append(avg_loss)
-        self.epochs.append(self.current_epoch)
+            # Following layers are now correctly placed after convolution as in the TensorFlow model
+            if batchnorm:
+                self.layers.append(nn.BatchNorm2d(n_filters*2))  # Assuming n_filters doubles after ConvComplex2D_py
 
-    # Check if the current epoch is a multiple of 10
-        if self.current_epoch % 10 == 0: 
-           plt.figure()  # Create a new figure
-           plt.plot(self.epochs, self.loss, 'ro-')
-           plt.title('Training loss')
-           plt.xlabel('Epoch')
-           plt.ylabel('Loss')
-        
-           # Ensure the directory for saving the plots exists
-           save_dir = "training_plot"
-           os.makedirs(save_dir, exist_ok=True)
-        
-           # Save the plot with a unique filename for each epoch
-           #plt.savefig(os.path.join(save_dir, f'training_loss_epoch_{self.current_epoch}.png'))
-        
-           # Clear the plot after saving to avoid memory issues with multiple plots
-           plt.close()
-    
-    def on_test_epoch_end(self, num_images_to_plot=10):
-        # Concatenate all targets and outputs
-        targets = torch.cat(self.targets, dim=0)
-        outputs = torch.cat(self.outputs, dim=0)
+            if activation is not None:
+                self.layers.append(activation())
 
-        # Convert tensors to numpy arrays for plotting
-        targets_np = targets.cpu().numpy()
-        outputs_np = outputs.cpu().numpy()
+            if dp_rate > 0.0:
+                self.layers.append(MonteCarloDropout(dp_rate))
 
-        # Save images
-        self.save_images(targets_np, outputs_np, num_images_to_plot, "test_image")
-        #clear the lists
-        self.targets=[]
-        self.outputs=[]
-
-    
-    
-    def on_validation_epoch_end(self,num_images_to_plot=10):
-        #print("Validation epoch end")
-        #only for each 10th epoch
-        if self.current_epoch % 10 == 0:
-            # Concatenate all targets and outputs
-            targets = torch.cat(self.targets, dim=0)
-            outputs = torch.cat(self.outputs, dim=0)
-
-            # Convert tensors to numpy arrays for plotting
-            targets_np = targets.cpu().numpy()
-            outputs_np = outputs.cpu().numpy()
-            #print(f"Targets: {targets_np.shape}, Outputs: {outputs_np.shape}")
-
-            # Save images
-            #foldername should be unique for each epoch
-            self.save_images(targets_np, outputs_np, num_images_to_plot, f"validation_image_{self.current_epoch}")           
-            #clear the lists
-            self.targets=[]
-            self.outputs=[]
-            #priint count of tartgets and outputs
-            #print(f"Targets: {len(self.targets)}, Outputs: {len(self.outputs)}")
-        
-
-
-
-    def save_images(self, targets_np, outputs_np, num_images_to_plot, save_dir):
-        # Determine how many images to save
-        num_images_to_plot = min(num_images_to_plot, len(targets_np))
-
-        # Randomly select indices to save
-        indices_to_save = np.random.choice(len(targets_np), num_images_to_plot, replace=False)
-
-        # Ensure the directory for saving the images exists
-        os.makedirs(save_dir, exist_ok=True)
-
-        # Loop through the selected indices and save each image
-        for i in indices_to_save:
-            # Normalize the target and output images before saving
-            target_img = normalize_image(targets_np[i][0])  # Assuming single-channel images
-            output_img = normalize_image(outputs_np[i][0])
-
-            # Save the target and output images directly without scaling them to 255
-            plt.imsave(os.path.join(save_dir, f"target_{i}.png"), target_img, cmap='gray', format='png')
-            plt.imsave(os.path.join(save_dir, f"output_{i}.png"), output_img, cmap='gray', format='png')
-
-        # Plot the first N images
-        fig, axes = plt.subplots(num_images_to_plot, 2, figsize=(10, num_images_to_plot * 2))
-        for j in range(num_images_to_plot):
-            index = indices_to_save[j]
-            axes[j, 0].imshow(normalize_image(targets_np[index][0]), cmap='gray')
-            axes[j, 0].set_title('Ground Truth')
-            axes[j, 0].axis('off')
-
-            axes[j, 1].imshow(normalize_image(outputs_np[index][0]), cmap='gray')
-            axes[j, 1].set_title('Prediction')
-            axes[j, 1].axis('off')
-
-        plt.tight_layout()
-        # Optionally, save the figure to a file
-        fig.savefig(os.path.join(save_dir, f'gt_vs_pred_{num_images_to_plot}.png'))
-        plt.close(fig)
+            in_channels = n_filters # Adjust for the next layer, if any
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
