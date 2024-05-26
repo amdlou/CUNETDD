0cc0a32f4936e1219bd196dd18ef47242b5aaa17
amin <Amin.Poozesh.2020@gmail.com>
2024-05-25 20:34:18 -0500

    adding Attention gate and positional embedding

--- model.py
+++ model.py
@@ -12,9 +12,129 @@
 from typing import Optional, Type
 import torch
 from torch import nn
+import torch.nn.functional as F
 import numpy as np
 from utils import cross_correlate_fft, cross_correlate_ifft
 from utils import ConvSpec2D, Conv2D, ComplexUpsample2d
+
+class AttentionGate(nn.Module):
+    """
+    AttentionGate module that performs attention mechanism on input tensors.
+
+    Args:
+        in_channels (int): Number of input channels.
+        gating_channels (int): Number of gating channels.
+        inter_channels (int, optional): Number of intermediate channels. Defaults to None.
+
+    Attributes:
+        in_channels (int): Number of input channels.
+        gating_channels (int): Number of gating channels.
+        inter_channels (int): Number of intermediate channels.
+        W_g (nn.Sequential): Sequential module for gating channel convolution.
+        W_x (nn.Sequential): Sequential module for input channel convolution.
+        psi (nn.Sequential): Sequential module for sigmoid activation.
+        relu (nn.ReLU): ReLU activation function.
+
+    """
+
+    def __init__(self, in_channels, gating_channels, inter_channels=None):
+        super().__init__()
+
+        self.in_channels = in_channels
+        self.gating_channels = gating_channels
+
+        self.inter_channels = in_channels // 2 if inter_channels is None else inter_channels
+
+        self.W_g = nn.Sequential(
+            ConvSpec2D(in_channels=self.gating_channels, n_filters=self.in_channels),
+        )
+
+        self.W_x = nn.Sequential(
+            #Conv2D(in_channels=self.in_channels, n_filters=self.in_channels),  # Add this line
+            ConvSpec2D(in_channels=self.gating_channels, n_filters=self.in_channels),
+        )
+
+        self.psi = nn.Sequential(
+            ConvSpec2D(in_channels=self.in_channels, n_filters=1),
+            nn.Sigmoid()
+        )
+
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, g, x):
+        """
+        Forward pass of the AttentionGate module.
+
+        Args:
+            g (torch.Tensor): Gating tensor.
+            x (torch.Tensor): Input tensor.
+
+        Returns:
+            torch.Tensor: Output tensor after attention mechanism.
+
+        """
+        shape_x = x.size()
+        g1 = self.W_g(g)
+        x1 = self.W_x(x)
+        psi = self.relu(g1 + x1)
+        psi = self.psi(psi)
+        upsample_psi = F.interpolate(psi, size=(shape_x[2], shape_x[3]), mode='bilinear', align_corners=True)
+        real_psi, imag_psi = torch.chunk(upsample_psi, 2, dim=1)
+        real_x, imag_x = torch.chunk(x, 2, dim=1)
+        real_product = real_psi * real_x
+        imag_product = imag_psi * imag_x
+        x=torch.cat((real_product, imag_product), dim=1)
+
+        return x
+
+class RelativePositionalEmbedding(nn.Module):
+    """
+    Initialize the RelativePositionalEmbedding module.
+
+    Args:
+        d_model (int): The dimension of the input and output tensors.
+        max_len (int, optional): The maximum length of the input sequence. Defaults to 5000.
+    """
+    def __init__(self, d_model, max_len=5000):
+
+        super().__init__()
+        self.d_model = d_model
+        self.max_len = max_len
+        self.embedding = nn.Embedding(2*max_len-1, d_model)
+
+    def forward(self, x):
+        """
+        Forward pass of the model.
+
+        Args:
+            x (torch.Tensor): Input tensor of shape (batch_size, seq_len, input_dim).
+
+        Returns:
+            torch.Tensor: Output tensor of shape (batch_size, seq_len, input_dim).
+        """
+        seq_len = x.size(1)
+        position_matrix = self.generate_relative_positions_matrix(seq_len, self.max_len)
+        position_matrix = position_matrix.to(x.device)
+        embeddings = self.embedding(position_matrix)
+        return x + embeddings
+
+    def generate_relative_positions_matrix(self, length, max_length):
+        """
+        Generate a matrix of relative positions.
+
+        Args:
+            length (int): The length of the sequence.
+            max_length (int): The maximum length of the sequence.
+
+        Returns:
+            torch.Tensor: A matrix of relative positions.
+        """
+        range_vec = torch.arange(length)
+        range_mat = range_vec[None, :].expand(length, length)
+        distance_mat = range_mat - torch.t(range_mat)
+        distance_mat_clipped = torch.clamp(distance_mat, -max_length, max_length)
+        final_mat = distance_mat_clipped + max_length - 1
+        return final_mat
 
 
 class ComplexUNet(nn.Module):
@@ -43,7 +163,7 @@ class ComplexUNet(nn.Module):
                  batchnorm: bool = True,
                  bias: bool = True) -> None:
 
-        super(ComplexUNet, self).__init__()
+        super().__init__()
         self.cross_correlate = cross_correlate_fft
         self.inverse_fft = cross_correlate_ifft
         self.initial_conv = ConvSpec2D(input_channel, filter_size, 3,
@@ -51,12 +171,15 @@ class ComplexUNet(nn.Module):
                                        bias, batchnorm)
         self.encoder = nn.ModuleList()
         self.decoder = nn.ModuleList()
+        self.pos_embedding = RelativePositionalEmbedding(d_model=256)
         current_channels = filter_size
         max_channels = 256
         current_image_size = image_size
+        self.attention_blocks = nn.ModuleList()
 
         # Encoder - Convolution followed by Pooling
         for _ in range(int(np.log2(image_size)) - 1):
+            self.attention_blocks = nn.ModuleList()
             next_channels = min(current_channels * 2, max_channels)
             self.encoder.append(ConvSpec2D(current_channels, next_channels, 3,
                                            n_depth, activation, dp_rate, bias,
@@ -88,10 +211,11 @@ class ComplexUNet(nn.Module):
                                                n_depth, activation, dp_rate,
                                                bias, batchnorm))
                 self.decoder.append(ComplexUpsample2d(scale_factor=2,
-                                                      mode='bilinear'))
+                                                     mode='bilinear'))
+            self.attention_blocks.append(AttentionGate(current_channels, upsample_channels))  # Add this line
             current_channels = upsample_channels
             current_image_size *= 2
-
+        
         self.additional_conv = ConvSpec2D(current_channels, filter_size, 3,
                                           n_depth, activation, dp_rate,
                                           bias, batchnorm)
@@ -114,7 +238,10 @@ class ComplexUNet(nn.Module):
         Returns:
             torch.Tensor: Output tensor after passing through the CUNETD model.
         """
-        x = self.cross_correlate(inputsa, inputsb)
+        inputsaa = self.pos_embedding(inputsa)
+        inputsbb = self.pos_embedding(inputsb)
+        x = self.cross_correlate(inputsaa, inputsbb)
+
         x = self.initial_conv(x)
         skips = []
 
@@ -133,6 +260,8 @@ class ComplexUNet(nn.Module):
             x = self.decoder[i](x)  # Convolution
             x = self.decoder[i + 1](x)  # Upsampling
             skip_connection = skips.pop()
+            x = self.attention_blocks[i // 2](x, skip_connection)
+            
             x = torch.cat((x, skip_connection), dim=1)
         # Last decoder block
         x = self.decoder[-2](x)  # Last Convolution
@@ -143,3 +272,4 @@ class ComplexUNet(nn.Module):
         x = self.final_conv(x)
         x = self.actv(x)
         return x
+    
