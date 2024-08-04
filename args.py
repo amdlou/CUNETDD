253b2547d5a65baaa66970540f5b6c02793e7397
amin <Amin.Poozesh.2020@gmail.com>
2024-08-04 13:00:04 -0500

    Final

--- args.py
+++ args.py
@@ -22,7 +22,7 @@ def get_args() -> Dict[str, Any]:
 
         'input_channel': 1,  # Set the number of input channels
         'image_size': 256,  # Set the size of the input images
-        'batch_size': 32,  # Set the batch size
+        'batch_size': 8,  # Set the batch size
         'filter_size': 8,  # Set the initial number of filters
         'n_depth': 1,  # Set the depth of the network
         'dp_rate': 0.1,  # Set the dropout rate
@@ -31,27 +31,27 @@ def get_args() -> Dict[str, Any]:
         'shuffle': True,  # Set to False to disable shuffling
         'drop_last': True,  # Set to False to keep the last batch
         'pin_memory': False,  # Set to True to use pinned memory
-        'persistent_workers': True,  # Set to True to use persistent workers
-        'plot_frequency': 50,  # Set the frequency of plotting
+        'persistent_workers': False,  # Set to True to use persistent workers
+        'plot_frequency': 1,  # Set the frequency of plotting
         'num_images_to_plot': 4,  # Set the number of images to plot
-        'num_workers': 9,  # Set the number of workers for data loading
+        'num_workers': 0,  # Set the number of workers for data loading
 
 
         # #### Trainer arguments#####
 
-        'gpus': None, #[0,1],  # Set to None for CPU
+        'gpus': None,  #[0,1],  # Set to None for CPU
         'strategy': 'ddp',  # Set the strategy for distributed training
         'num_nodes': 1,  # Set the number of nodes
         'mode': 'fit',  # Set to 'fit' for training, 'test' for testing
-        'max_epochs': 3,  # Set the maximum number of epochs
+        'max_epochs': 5,  # Set the maximum number of epochs
         'accumulate_grad_batches': 16 ,  # Set the number of batches to accumulate
-        'limit_train_batches' : 1.0,  # Set the fraction of training data to us
+        'limit_train_batches' : 0.4,  # Set the fraction of training data to us
         'track_grad_norm': -1,  # Set the norm to track
         'gradient_clip_val': 0.5,  # Set the value for gradient clipping
         'fast_dev_run': False,  # Set to True for a quick test run
         'use_profiler': False,  # Set to True to use profiler, False to not use
-        'log_every_n_steps': 100,  # Set the number of steps between each log
-        'check_val_every_n_epoch': 1,  # Set the frequency of validation
+        'log_every_n_steps': 1,  # Set the number of steps between each log
+        'check_val_every_n_epoch': 2,  # Set the frequency of validation
         'precision': '16',  # Set to 'mixed' to enable mixed training
         'benchmark': True,  # Set to True to enable benchmarking
         'gradient_clip_algorithm': 'value',  # Set the algorithm for gradient clipping
@@ -61,11 +61,11 @@ def get_args() -> Dict[str, Any]:
 
 
         # #### Directory arguments####
-
+        'image_folder_name': 'validation_image1',  # Set the name of the main folder
         'checkpoint_dir': './',  # Set the directory for saving checkpoints
-        'train_dataset_dir': './train', #'/ourdisk/hpc/disc/amin/auto_archive_notyet/tape_2copies/4DSTEM_DATA/rotated_data',  # Add the directory for the training
+        'train_dataset_dir': './train',  #'/ourdisk/hpc/disc/amin/auto_archive_notyet/tape_2copies/4DSTEM_DATA/rotated_data',  # Add the directory for the training
         'test_dataset_dir': './test',  #Addthe directory for the test
-        'val_dataset_dir': './val', #  '/ourdisk/hpc/disc/amin/auto_archive_notyet/tape_2copies/val/rotate',  # Add the directory for the validation
-        'checkpoint_pth': None, #'./FCUnet-epoch=798.ckpt',  # Set to the path of the checkpoint to laod or None
+        'val_dataset_dir': './val',  #  '/ourdisk/hpc/disc/amin/auto_archive_notyet/tape_2copies/val/rotate',  # Add the directory for the validation
+        'checkpoint_pth': None,  #'./FCUnet-epoch=798.ckpt',  # Set to the path of the checkpoint to laod or None
     }
     return args
