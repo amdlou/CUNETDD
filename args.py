da306daf08f8cf2a2170a9cac01825634b468f34
amdliman <149844881+amdliman@users.noreply.github.com>
2024-08-17 23:01:51 -0500

    Update args.py

--- args.py
+++ args.py
@@ -1,7 +1,6 @@
 """
 This file contains the arguments for the CUNETD model.
 """
-
 from typing import Dict, Any
 from torch import nn
 
@@ -15,44 +14,56 @@ def get_args() -> Dict[str, Any]:
         args (Dict[str, Any]): A dictionary containing the arguments for
         the CUNETD model.
     """
+
     args: Dict[str, Any] = {
 
-        # #### Main arguments #####
+        # #### Model arguments #####
 
         'input_channel': 1,  # Set the number of input channels
         'image_size': 256,  # Set the size of the input images
-        'batch_size': 2,  # Set the batch size
-        'filter_size': 4,  # Set the initial number of filters
-        'n_depth': 1,  # Set the depth of the network
-        'dp_rate': 0.3,  # Set the dropout rate
-        'num_workers': 4,  # Set the number of workers for data loading
-        'plot_frequency': 10,  # Set the frequency of plotting
-        'num_images_to_plot': 4,  # Set the number of images to plot
-        'learning_rate': 0.001,  # Add the learning rate
-        'log_every_n_steps': 50,  # Set the number of steps between each log
-        'max_epochs': 10,  # Set the maximum number of epochs
-
-        # #### Optional arguments#####
-
-        'shuffle': True,  # Set to False to disable shuffling
-        'fast_dev_run': False,  # Set to True for a quick test run
-        'use_profiler': False,  # Set to True to use profiler, False to not use
-        'sync_bnorm': False,  # Set to True to sync batch norm across GPUs
-        'gpus': None,  # Set to None for CPU
-        'execution_mode': 'fit',
-        # Set to 'fit' for training, 'test' for testing
+        'batch_size': 32,  # Set the batch size
+        'filter_size': 16,  # Set the initial number of filters
+        'n_depth': 3,  # Set the depth of the network
+        'dp_rate': 0.1,  # Set the dropout rate
+        'learning_rate': 0.0001,  # Add the learning rate
         'activation': nn.ReLU,  # Note: Use the module directly
-        'chek_val_every_n_epoch': 1,  # Set the frequency of validation
+        'shuffle': True,  # Set to False to disable shuffling
+        'drop_last': True,  # Set to False to keep the last batch
         'pin_memory': True,  # Set to True to use pinned memory
         'persistent_workers': True,  # Set to True to use persistent workers
+        'plot_frequency': 10,  # Set the frequency of plotting
+        'num_images_to_plot': 4,  # Set the number of images to plot
+        'num_workers': 2,  # Set the number of workers for data loading
 
 
-        # #### Directory arguments#####
+        # #### Trainer arguments#####
 
-        'checkpoint_pth': None,  # Set to the path of the checkpoint to laod
+        'gpus': -1,  #[0,1],  # Set to None for CPU
+        'strategy': 'ddp_find_unused_parameters_true',  # Set the strategy for distributed training
+        'num_nodes': 5,  # Set the number of nodes
+        'mode': 'fit',  # Set to 'fit' for training, 'test' for testing
+        'max_epochs': 2,  # Set the maximum number of epochs
+        'accumulate_grad_batches': 16,  # Set the number of batches to accumulate
+        'limit_train_batches' : 0.98,  # Set the fraction of training data to us
+        'track_grad_norm': -1,  # Set the norm to track
+        'gradient_clip_val': 0.5,  # Set the value for gradient clipping
+        'fast_dev_run': False,  # Set to True for a quick test run
+        'use_profiler': False,  # Set to True to use profiler, False to not use
+        'log_every_n_steps': 1,  # Set the number of steps between each log
+        'check_val_every_n_epoch': 2,  # Set the frequency of validation
+        'precision': '16',  # Set to 'mixed' to enable mixed training
+        'benchmark': True,  # Set to True to enable benchmarking
+        'gradient_clip_algorithm': 'value',  # Set the algorithm for gradient clipping
+        'deterministic': False,  # Set to True to enable deterministic training
+        'enable_progress_bar': False,  # Set to True to enable progress bar
+        'sync_bnorm': True,  # Set to True to sync batch norm across GPUs
+
+
+        # #### Directory arguments####
+
+        'image_folder_name': 'validation_image1',  # Set the name of the main folder
         'checkpoint_dir': './',  # Set the directory for saving checkpoints
-        'train_dataset_dir': './train',  # Add the directory for the training
-        'test_dataset_dir': './test',  # Add the directory for the test
-        'val_dataset_dir': './val',  # Add the directory for the validation
+        'train_dataset_dir': '/pscratch/sd/i/iman/datasets/rotated_data/ml-ai-4dstem-main-rotated',   # Add the directory for the training
+        'checkpoint_pth': None,  # './FCUnet-epoch=798.ckpt',  # Set to the path of the checkpoint to laod or None
     }
     return args
