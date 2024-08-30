ef7356fd2ec1e3844a45c77af7b8185bf8b618dd
AminPoozesh <147357140+ai-amin@users.noreply.github.com>
2024-08-30 17:33:32 -0500

    Update args.py

--- args.py
+++ args.py
@@ -2,6 +2,7 @@
 This file contains the arguments for the CUNETD model.
 """
 from typing import Dict, Any
+import argparse
 from torch import nn
 
 
@@ -21,36 +22,34 @@ def get_args() -> Dict[str, Any]:
 
         'input_channel': 1,  # Set the number of input channels
         'image_size': 256,  # Set the size of the input images
-        'batch_size': 32,  # Set the batch size
+        'batch_size': 8,  # Set the batch size
         'filter_size': 16,  # Set the initial number of filters
-        'n_depth': 3,  # Set the depth of the network
-        'dp_rate': 0.1,  # Set the dropout rate
-        'learning_rate': 0.0001,  # Add the learning rate
+        'n_depth': 4,  # Set Number of convolutional layers in each block
+        'dp_rate': 0.5,  # Set the dropout rate
+        'learning_rate': 0.001,  # Add the learning rate
         'activation': nn.ReLU,  # Note: Use the module directly
         'shuffle': True,  # Set to False to disable shuffling
         'drop_last': True,  # Set to False to keep the last batch
-        'pin_memory': True,  # Set to True to use pinned memory
-        'persistent_workers': True,  # Set to True to use persistent workers
+        'pin_memory': False,  # Set to True to use pinned memory
+        'persistent_workers': False,  # Set to True to use persistent workers
         'plot_frequency': 10,  # Set the frequency of plotting
         'num_images_to_plot': 4,  # Set the number of images to plot
-        'num_workers': 2,  # Set the number of workers for data loading
-
+        'num_workers': 0,  # Use the parsed num_workers
 
         # #### Trainer arguments#####
-
-        'gpus': -1,  #[0,1],  # Set to None for CPU
-        'strategy': 'ddp_find_unused_parameters_true',  # Set the strategy for distributed training
-        'num_nodes': 5,  # Set the number of nodes
+        'num_nodes': 1,  # Use the parsed num_nodes
+        'gpus': None,  # Set to None for CPU
+        'strategy': 'ddp',  # Set the strategy for distributed training
         'mode': 'fit',  # Set to 'fit' for training, 'test' for testing
-        'max_epochs': 2,  # Set the maximum number of epochs
+        'max_epochs': 200,  # Set the maximum number of epochs
         'accumulate_grad_batches': 16,  # Set the number of batches to accumulate
-        'limit_train_batches' : 0.98,  # Set the fraction of training data to us
+        'limit_train_batches': 1.0,  # Set the fraction of training data
         'track_grad_norm': -1,  # Set the norm to track
         'gradient_clip_val': 0.5,  # Set the value for gradient clipping
         'fast_dev_run': False,  # Set to True for a quick test run
         'use_profiler': False,  # Set to True to use profiler, False to not use
-        'log_every_n_steps': 1,  # Set the number of steps between each log
-        'check_val_every_n_epoch': 2,  # Set the frequency of validation
+        'log_every_n_steps': 10,  # Set the number of steps between each log
+        'check_val_every_n_epoch': 1,  # Set the frequency of validation
         'precision': '16',  # Set to 'mixed' to enable mixed training
         'benchmark': True,  # Set to True to enable benchmarking
         'gradient_clip_algorithm': 'value',  # Set the algorithm for gradient clipping
@@ -63,7 +62,7 @@ def get_args() -> Dict[str, Any]:
 
         'image_folder_name': 'validation_image1',  # Set the name of the main folder
         'checkpoint_dir': './',  # Set the directory for saving checkpoints
-        'train_dataset_dir': '/pscratch/sd/i/iman/datasets/rotated_data/ml-ai-4dstem-main-rotated',   # Add the directory for the training
-        'checkpoint_pth': None,  # './FCUnet-epoch=798.ckpt',  # Set to the path of the checkpoint to laod or None
+        'train_dataset_dir': './train',  # '/ourdisk/hpc/disc/amin/auto_archive_notyet/tape_2copies/4DSTEM_DATA/rotated_data',  # Add the directory for the training
+        'checkpoint_pth': None, #'./FCUnet-epoch=69.ckpt',  # './FCUnet-epoch=798.ckpt',  # Set to the path of the checkpoint to laod or None
     }
     return args
