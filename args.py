f7a36c46fac77b8d9edbbd220ffd32fb67ecadf8
AminPoozesh <147357140+ai-amin@users.noreply.github.com>
2024-08-30 18:08:00 -0500

    Update args.py

--- args.py
+++ args.py
@@ -15,30 +15,34 @@ def get_args() -> Dict[str, Any]:
         args (Dict[str, Any]): A dictionary containing the arguments for
         the CUNETD model.
     """
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_nodes', type=int, default=1, help='Set the number of nodes')
+    parser.add_argument('--num_workers', type=int, default=0, help='Set the number of workers for data loading')
+    parsed_args = parser.parse_args()
+    
     args: Dict[str, Any] = {
 
         # #### Model arguments #####
 
         'input_channel': 1,  # Set the number of input channels
         'image_size': 256,  # Set the size of the input images
-        'batch_size': 8,  # Set the batch size
-        'filter_size': 16,  # Set the initial number of filters
-        'n_depth': 4,  # Set Number of convolutional layers in each block
-        'dp_rate': 0.5,  # Set the dropout rate
+        'batch_size': 16,  # Set the batch size
+        'filter_size': 32,  # Set the initial number of filters
+        'n_depth': 3,  # Set Number of convolutional layers in each block
+        'dp_rate': 0.3,  # Set the dropout rate
         'learning_rate': 0.001,  # Add the learning rate
         'activation': nn.ReLU,  # Note: Use the module directly
         'shuffle': True,  # Set to False to disable shuffling
         'drop_last': True,  # Set to False to keep the last batch
-        'pin_memory': False,  # Set to True to use pinned memory
-        'persistent_workers': False,  # Set to True to use persistent workers
+        'pin_memory': True,  # Set to True to use pinned memory
+        'persistent_workers': True,  # Set to True to use persistent workers
         'plot_frequency': 10,  # Set the frequency of plotting
         'num_images_to_plot': 4,  # Set the number of images to plot
-        'num_workers': 0,  # Use the parsed num_workers
+        'num_workers': parsed_args.num_workers,  # Use the parsed num_workers
 
         # #### Trainer arguments#####
-        'num_nodes': 1,  # Use the parsed num_nodes
-        'gpus': None,  # Set to None for CPU
+        'num_nodes': parsed_args.num_nodes,  # Use the parsed num_nodes
+        'gpus': -1,  # Set to None for CPU
         'strategy': 'ddp',  # Set the strategy for distributed training
         'mode': 'fit',  # Set to 'fit' for training, 'test' for testing
         'max_epochs': 200,  # Set the maximum number of epochs
@@ -60,7 +64,7 @@ def get_args() -> Dict[str, Any]:
 
         # #### Directory arguments####
 
-        'image_folder_name': 'validation_image1',  # Set the name of the main folder
+        'image_folder_name': 'validation_image',  # Set the name of the main folder
         'checkpoint_dir': './',  # Set the directory for saving checkpoints
         'train_dataset_dir': './train',  # '/ourdisk/hpc/disc/amin/auto_archive_notyet/tape_2copies/4DSTEM_DATA/rotated_data',  # Add the directory for the training
         'checkpoint_pth': None, #'./FCUnet-epoch=69.ckpt',  # './FCUnet-epoch=798.ckpt',  # Set to the path of the checkpoint to laod or None
