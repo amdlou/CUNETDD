5acdb4297c054286ed33a83d334c13639964b050
AminPoozesh <147357140+ai-amin@users.noreply.github.com>
2024-09-06 22:29:16 -0500

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -189,9 +189,7 @@ class ParseDataset(Dataset):
             data_meas = torch.from_numpy(file['dataMeas'][..., idx])
             data_probe = torch.from_numpy(file['dataProbe'][...])
             data_pots = torch.from_numpy(file['dataPots'][..., idx])
-            data_pots = min_max_normalize(data_pots)
-            #data_pots = torch.log(data_pots + 1e-6)
-            #data_pots = filter_hot_pixels_pytorch(data_pots.unsqueeze(0), thresh=0.5, ind_compare=1).squeeze(0)
+
         cbed = data_meas.unsqueeze(0)
         probe = data_probe.unsqueeze(0)
         pot = data_pots.unsqueeze(0)
